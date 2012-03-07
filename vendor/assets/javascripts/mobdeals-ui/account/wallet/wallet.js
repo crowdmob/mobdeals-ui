9aeb1c79fe95a0cdad08af7c63ce8ed66953473d
Matthew Moore <matt@crowdmob.com>
2012-03-07 00:14:04 -0800

    paypal gets redirect info

--- vendor/assets/javascripts/mobdeals-ui/account/wallet/wallet.js
+++ vendor/assets/javascripts/mobdeals-ui/account/wallet/wallet.js
@@ -68,9 +68,8 @@ MobDeals.Account.Wallet = {
     });
   },
   _startPaypal: function(callback, offer) { console.log("offer", offer);
-    $.get(MobDeals.host('core')+'/account/wallet/methods/paypals/new.json?purchasable_type='+offer.purchasable_type+'&purchasable_id='+offer.id, function(data) {
+    $.get(MobDeals.host('core')+'/account/wallet/methods/paypals/new.json?settle=true&redirect=offerwall&habitat[apikey]='+MobDeals.Habitat.apikey()+'&purchasable_type='+offer.purchasable_type+'&purchasable_id='+offer.id, function(data) {
       MobDeals.redirect(data.setup_url);
-      //      else { MobDeals.Popup.destroy(popup); if (callback) { callback.apply(callback, [data, true]); } }
     }, 
     'json');
   },
