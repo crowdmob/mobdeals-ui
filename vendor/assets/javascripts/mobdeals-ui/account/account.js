a10c46801307b421d0dfdc3e747a3c4cde5b248f
Matthew Moore <matt@crowdmob.com>
2012-03-14 17:18:40 -0700

    remove username

--- vendor/assets/javascripts/mobdeals-ui/account/account.js
+++ vendor/assets/javascripts/mobdeals-ui/account/account.js
@@ -76,7 +76,7 @@ MobDeals.Account = {
 
   _username: function(parent, callback, error) {
     var input = parent.get(0).nodeName.toLowerCase() == 'input' ? parent : parent.find('input');
-    var params = {}; params[input.get(0).name] = input.val(); params['user[username]'] = input.val();
+    var params = {}; params[input.get(0).name] = input.val();
     var setAndCallback = function(dataOrXhr, error, errorType) { console.log("GOT USER from callback:", dataOrXhr, error, errorType);
       if (error && error != 'success') {
         MobDeals.Account.prompt(callback, $.parseJSON(dataOrXhr.responseText));
