3191ac75dbdd0bc136cca83b0fba1cafb6ecb665
Rohen Peterson <rohen@crowdmob.com>
2012-03-17 02:16:41 -0700

    trying a higher timeout for jquery

--- vendor/assets/javascripts/mobdeals-ui/account/account.js
+++ vendor/assets/javascripts/mobdeals-ui/account/account.js
@@ -94,7 +94,7 @@ MobDeals.Account = {
       url: MobDeals.host('core')+'/users/sign_in.json', 
       type: 'POST',
       data: params,
-      timeout: 10,
+      timeout: 1000,
       success: function(data) { console.log('got session data', data);
         console.log("Successful ajax call!");
         if (data.password_initialized) {
