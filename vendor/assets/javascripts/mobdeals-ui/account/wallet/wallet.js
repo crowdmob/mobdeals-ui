f0fbe001690e292babeaa3d3e3cd26e6cfc0fa41
Matthew Moore <matt@crowdmob.com>
2012-03-17 11:01:42 -0700

    ew, moved selected virtual good to purchasable, not habitat

--- vendor/assets/javascripts/mobdeals-ui/account/wallet/wallet.js
+++ vendor/assets/javascripts/mobdeals-ui/account/wallet/wallet.js
@@ -69,8 +69,8 @@ MobDeals.Account.Wallet = {
   },
   _start3rdParty: function(uri, callback, offer) {
     $.get(MobDeals.host('core') + 
-      '/account/wallet/methods/' + uri + '/new.json?settle=true&redirect=offerwall&habitat[virtual_good_id]='+offer.virtual_good_id+'&habitat[apikey]=' + 
-      MobDeals.Habitat.apiKey() + '&purchasable_type=' + offer.purchasable_type + '&purchasable_id=' + offer.id, 
+      '/account/wallet/methods/' + uri + '/new.json?settle=true&redirect=offerwall&habitat[apikey]=' + 
+      MobDeals.Habitat.apiKey() + '&purchasable_type=' + offer.purchasable_type + '&purchasable_id=' + offer.id+ '&purchasable[virtual_good_id]='+offer.virtual_good_id, 
       function(data) {
         MobDeals.redirect(data.setup_url);
       }, 
