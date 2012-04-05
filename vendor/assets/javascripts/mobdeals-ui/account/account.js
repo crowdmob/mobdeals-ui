62132384097e3506b03e0da3430fbb835e27d281
Rohen Peterson <rohen@crowdmob.com>
2012-04-04 18:32:16 -0700

    testing with input submit for enter

--- vendor/assets/javascripts/mobdeals-ui/account/account.js
+++ vendor/assets/javascripts/mobdeals-ui/account/account.js
@@ -36,18 +36,18 @@ MobDeals.Account = {
       popup.find('a.email').bind(CLICK, function(ev) {
  	      popup.find('.inputs').slideDown();
         popup.find('.email-box').removeClass('hidden').addClass('active').find('form').submit(readInputCancelBubble).find('input').blur(readInput).focus(); 
+        popup.find('.email-box').keypress(function(event){
+          if(event.keyCode == 13){
+            $this.find('form').submit(readInputCancelBubble).find('input').blur(readInput).focus();
+          }
+        });
         popup.find('.mobile-box').addClass('hidden').removeClass('active');
-        //popup.find('.email-box').keyup(function(event){
-        //  if(event.keyCode == 13){
-        //    popup.find('.email-box').find('form').submit(readInputCancelBubble).find('input').blur(readInput).focus();
-        //  }
-        //});
       });
       popup.find('a.mobile').bind(CLICK, function(ev) { 
  	      popup.find('.inputs').slideDown();
         popup.find('.mobile-box').removeClass('hidden').addClass('active').find('form').submit(readInputCancelBubble).find('input').blur(readInput).focus();
         popup.find('.email-box').addClass('hidden').removeClass('active');
-        //popup.find('.mobile-box').keyup(function(event){
+        //popup.find('.mobile-box').keypress(function(event){
         //  if(event.keyCode == 13){
         //    popup.find('.mobile-box').find('form').submit(readInputCancelBubble).find('input').blur(readInput).focus();
         //  }
