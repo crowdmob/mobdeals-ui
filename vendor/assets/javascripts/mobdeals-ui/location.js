3cda925ebdbf58663dcf61e163a7127c6f53393f
Rohen Peterson <rohen@crowdmob.com>
2012-04-17 03:40:49 -0700

    testing code

--- vendor/assets/javascripts/mobdeals-ui/location.js
+++ vendor/assets/javascripts/mobdeals-ui/location.js
@@ -5,14 +5,18 @@ MobDeals.Location = {
     if (this._initialized) { return false; } else { this._initialized = true; }
   },
   get: function(callback, forceRefresh) {
-    if (this.location && !forceRefresh) { return this.location; }
+    if (this.location && !forceRefresh) { 
+      console.log("HAS A LOCATION");
+      return this.location; }
     if (navigator.geolocation) {
+      console.log("GEOLOCATING");
       navigator.geolocation.getCurrentPosition(
         function(position) {
+          console.log("GETTING CURRENT LOCATION 1");
           MobDeals.Location.location = [position.coords.latitude, position.coords.longitude];
           callback.apply(callback, [MobDeals.Location.location]);
         },
-        function (error) { callback.apply(callback, [MobDeals.Location.location]); },
+        function (error) { console.log("ERROR!"); callback.apply(callback, [MobDeals.Location.location]); },
         { timeout: (5 * 1000), maximumAge: (1000 * 60 * 15), enableHighAccuracy: true }
       );
     }
