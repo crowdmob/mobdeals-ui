2c6daeeecf781c26317a87fed40c9395224c3a8f
Rohen Peterson <rohen@crowdmob.com>
2012-06-06 10:02:07 -0700

    shifting to mac address for android messaging

--- vendor/assets/javascripts/mobdeals-ui/account/account.js
+++ vendor/assets/javascripts/mobdeals-ui/account/account.js
@@ -278,7 +278,7 @@ MobDeals.Account = {
       android_id: uuids['android_id'],
       android_serial_number: uuids['android_id'],
       android_telephony_id: uuids['android_telephony_id'],
-      encrypted_mac_address: uuids['encrypted_mac_address']
+      mac_address: uuids['mac_address']
     };
     MobDeals.Account._registerDevice(data);
   },
