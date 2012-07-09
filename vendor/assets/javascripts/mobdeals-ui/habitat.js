f1702600c1d89f3ac4b05083790065552291fd2f
Raj Shah <brainix@gmail.com>
2012-07-09 16:08:10 -0700

    cleaned up code

--- vendor/assets/javascripts/mobdeals-ui/habitat.js
+++ vendor/assets/javascripts/mobdeals-ui/habitat.js
@@ -2,12 +2,12 @@
 // within.
 
 MobDeals.Habitat = {
-  device_type: null,
-  platform: null,
-  udid: null,
-  udid_type: null,
   _apiKey: null,
   _appId: null,
+  platform: null,
+  device_type: null,
+  udid: null,
+  udid_type: null,
   _initialized: false,
 
   init: function() {
@@ -18,19 +18,18 @@ MobDeals.Habitat = {
       var iOS = navigator.platform.match(/(iPad|iPhone|iPod)/i) ? true : false;
       var android = navigator.userAgent.toLowerCase().match(/android/i) != null;
       
-      if (iOS) { MobDeals.Habitat.platform = 'ios'; }
-      else if (android) { MobDeals.Habitat.platform = 'android'; }
-      else { MobDeals.Habitat.platform = 'html5'; }
+      if (iOS) {
+        MobDeals.Habitat.platform = 'ios';
+      }
+      else if (android) {
+        MobDeals.Habitat.platform = 'android';
+      }
+      else {
+        MobDeals.Habitat.platform = 'html5';
+      }
       
       this._initialized = true;
     }
-  },
-
-  apiKey: function(val) {
-    if (val) {
-      MobDeals.Habitat._apiKey = val;
-    }
-    return MobDeals.Habitat._apiKey;
   },
 
   appId: function(val) {
@@ -44,15 +43,21 @@ MobDeals.Habitat = {
     return MobDeals.Habitat._appId;
   },
 
+  apiKey: function(val) {
+    if (val) {
+      MobDeals.Habitat._apiKey = val;
+    }
+    return MobDeals.Habitat._apiKey;
+  },
+
   report: function(iOSHeader, iOSStr, androidFunctionWrapper) {
     if (MobDeals.Habitat.platform == 'android') {
       try {
         androidFunctionWrapper();
       } catch(e) {
-        console.log(e.message);
+        // Ignored.
       }
-    }
-    else if (MobDeals.Habitat.platform == 'ios') {
+    } else if (MobDeals.Habitat.platform == 'ios') {
       var iframe = document.createElement("IFRAME");
       iframe.setAttribute("src", iOSHeader + ":" + iOSStr);
       document.documentElement.appendChild(iframe);
