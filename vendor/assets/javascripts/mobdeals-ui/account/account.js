7bf27b14902440d25c75d4b32e894c9374ed3111
Rohen Peterson <rohen@crowdmob.com>
2012-08-09 19:22:27 -0700

    added validator functions for e-mail and phone

--- vendor/assets/javascripts/mobdeals-ui/account/account.js
+++ vendor/assets/javascripts/mobdeals-ui/account/account.js
@@ -343,6 +343,17 @@ MobDeals.Account = {
       dataType: 'json',
       crossDomain: true
     });
+  },
+  
+  isValidEmail: function(s) {
+    // Determine whether or not the passed in string is a valid email address.  TODO: Move this into MobDeals.Account.
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(s);
+  },
+  
+  isValidPhone: function(s) {
+    // Determine whether or not the passed in string is a valid phone number.  TODO: Move this into MobDeals.Account.
+    var re = /^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})$/;
+    return re.test(s);
   }
-
 };
