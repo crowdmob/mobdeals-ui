5c92407731aac2e3e1c5ee404679265319b023e4
Rohen Peterson <rohen@crowdmob.com>
2012-10-04 16:08:08 -0700

    reverting testing code

--- vendor/assets/javascripts/mobdeals-ui/account/account.js
+++ vendor/assets/javascripts/mobdeals-ui/account/account.js
@@ -385,7 +385,6 @@ MobDeals.Account = {
       }
       if (data.udid) {
         MobDeals.Habitat.ios_udid = data.udid;
-        alert("got a UDID:" + data.udid);
       }
     }
     else if (data.platform == 'android') {
